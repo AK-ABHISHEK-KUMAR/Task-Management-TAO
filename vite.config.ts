633f794fbdc8871eefaabc0f877549d7ce060872
Abhishek Kumar <ak906626@gmail.com>
2025-02-02 12:26:59 +0530

    Set base path for deployment in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Task-Management-TAO/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
